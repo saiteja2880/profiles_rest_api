ef02741addcab528ba983e3d493dd621b4b6417f
saiteja2880 <saitejajasthi9174@gmail.com>
2023-07-07 09:32:30 +0530

    urls commit

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """test api view"""
+
+    def get(self, request, format=None):
+        """return a list of api view feautures"""
+
+        an_apiview =[
+            'Uses HTTP methods as function (get, put,post,delete)'
+
+        ]
+        return Response({'messages':'hello','an_apiview':an_apiview} )
+
+
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authentication',
+    'rest_framework.authtoken',
     'profiles_api',
 ]
 
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
