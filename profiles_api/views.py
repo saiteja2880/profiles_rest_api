445a137294b6c9bd02894db026d5430c168a7a4d
saiteja2880 <saitejajasthi9174@gmail.com>
2023-07-07 10:33:04 +0530

    adding put post delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """test api view"""
-
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         """return a list of api view feautures"""
 
@@ -14,5 +17,32 @@ class HelloApiView(APIView):
 
         ]
         return Response({'messages':'hello','an_apiview':an_apiview} )
+
+
+    def post(self, request):
+        """create a helli message with our name"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else :
+            return Response (
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        
+
+    def put(self, request, pk=0):
+        return Response({'method':'PUT'})
+    
+
+    def patch(self, request, pk=0):
+        return Response({'method':'PATCH'})
+    
+
+    def delete(self, request, pk=0):
+        return Response({'method':'DELETE'})
 
 
