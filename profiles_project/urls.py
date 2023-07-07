68ba21f7ce23206004498c482a47034ffb9167fc
saiteja2880 <saitejajasthi9174@gmail.com>
2023-07-07 14:31:40 +0530

    adding profile

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from profiles_api import views
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
